aa2ec01f4928becda3a0d4990aeaccfa3224f7b3
Kenny Katzgrau <katzgrau@gmail.com>
2012-12-31 17:39:02 -0500

    Version bump

--- broadstreet.php
+++ broadstreet.php
@@ -3,7 +3,7 @@
 Plugin Name: Broadstreet
 Plugin URI: http://broadstreetads.com
 Description: Integrate Broadstreet business directory and adserving power into your site
-Version: 1.3.2
+Version: 1.3.3
 Author: Broadstreet
 Author URI: http://broadstreetads.com
 */
