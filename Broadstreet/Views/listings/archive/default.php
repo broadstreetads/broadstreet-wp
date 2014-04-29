4d50e9ae9128e4c40c9da978f457dcbb3094a601
Kenny Katzgrau <katzgrau@gmail.com>
2014-04-29 12:15:28 -0400

    Fix Google Plus

--- Broadstreet/Views/listings/archive/default.php
+++ Broadstreet/Views/listings/archive/default.php
@@ -81,6 +81,9 @@
             <?php if($meta['bs_facebook']): ?>
             <a target="_blank" href="<?php echo $meta['bs_facebook'] ?>"><img src="<?php echo Broadstreet_Utility::getImageBaseURL().'facebook.png' ?>" alt="facebook" width="20" /></a>
             <?php endif; ?>
+            <?php if($meta['bs_gplus']): ?>
+            <a target="_blank" href="<?php echo $meta['bs_gplus'] ?>"><img src="<?php echo Broadstreet_Utility::getImageBaseURL().'google.png' ?>" alt="Google Plus" width="20" /></a>
+            <?php endif; ?>
             <?php if($meta['bs_yelp']): ?>
             <a target="_blank" href="<?php echo $meta['bs_yelp'] ?>"><img src="<?php echo Broadstreet_Utility::getImageBaseURL().'yelp.png' ?>" alt="yelp" width="20" /></a>
             <?php endif; ?>
