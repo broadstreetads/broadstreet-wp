b6fcdb446bda4cbca90415cd3d14e03c1c39124a
Eugene Fabrikant <eugene@bizyhood.com>
2015-04-17 16:02:06 -0400

    Fixed a few merge issues.

--- Bizyhood/Core.php
+++ Bizyhood/Core.php
@@ -331,7 +331,7 @@ class Bizyhood_Core
     public function postTemplate($content)
     {   
         global $post;
-        $api_url = Broadstreet_Utility::getOption(self::KEY_API_URL);
+        $api_url = Bizyhood_Utility::getApiUrl();
 
         # Override content for the view business page
         if ($post->post_name === 'bh-business')
