2be76a3e63a44af1ec919005ad5be1b4e99a54c2
Eugene Fabrikant <eugene@bizyhood.com>
2015-04-20 22:44:10 -0400

    Added category, hours, and phone/website display.

--- Bizyhood/Views/listings/single/default.php
+++ Bizyhood/Views/listings/single/default.php
@@ -15,62 +15,56 @@
     <?php endif; ?>
     </div><!-- /.col-md-2 -->
     <div class="bh_col-md-10 bh_business-details">
-            <div class="bh_row">
-                    <div class="bh_main-content bh_col-md-9">
-                        <h2><?php echo $business->name ?></h2>
-                        <?php if(false): ?>
-                        <ol class="bh_breadcrumb">
-                            <li>Services and Supplies</li>
-                            <li>Personal Care</li>
-                            <li>Spas</li>
-                        </ol>
+        <div class="bh_row">
+            <div class="bh_main-content bh_col-md-9">
+                <h2><?php echo $business->name ?></h2>
+                <?php if($business->categories): ?>
+                <ol class="bh_breadcrumb">
+                    <?php foreach($business->categories as $category): ?>
+                    <li><?php echo $category ?></li>
+                    <?php endforeach; ?>
+                </ol>
+                <?php endif; ?>
+                <?php if ( $business->description ) : ?>
+                <?php echo wpautop($business->description); ?>
+                <?php else : ?>
+                <p>No description available</p>
+                <?php endif ?>
+            </div><!-- /.col-md-9 -->
+            <div class="bh_details bh_col-md-3">
+                <div class="bh_section">
+                    <h5>Location</h5>
+                    <p><?php echo $business->address1 ?><br>
+                    <?php echo $business->locality ?>, <?php echo $business->region ?> <?php echo $business->postal_code ?><br>
+                    <a href="https://maps.google.com?daddr=<?php echo urlencode($business->address1) ?>+<?php echo urlencode($business->locality) ?>+<?php echo urlencode($business->region) ?>+<?php echo urlencode($business->postal_code) ?>" target="_blank">Get directions &rarr;</a></p>
+                </div>
+                <?php if($business->hours): ?>
+                <div class="bh_section">
+                    <h5>Hours</h5>
+                    <dl class="bh_dl-horizontal">
+                        <?php foreach($business->hours as $hour): ?>
+                        <dt><?php echo $hour->day_name; ?>:</dt>
+                        <dd><?php if($hour->hours_type == 1): ?><?php foreach($hour->hours as $hour_display): ?><?php echo date('g:i a',strtotime($hour_display[0])); ?>&ndash;<?php echo date('g:i a',strtotime($hour_display[1])); ?> <?php endforeach; ?><?php else : ?><?php echo $hour->hours_type_name; ?><?php endif; ?></dd>
+                        <?php endforeach; ?>
+                    </dl>
+                </div>
+                <?php endif; ?>
+                <?php if($business->telephone): ?>
+                <div class="bh_section">
+                    <h5>Contact Us</h5>
+                    <div class="bh_list-group">
+                        <a class="bh_list-group-item">Phone: <?php echo $business->telephone; ?></a>
+                        <?php if($business->website): ?>
+                        <a class="bh_list-group-item" href="<?php echo $business->website; ?>" target="_blank">Visit our Website &rarr;</a>
                         <?php endif; ?>
-                        <?php if ( $business->description ) : ?>
-                        <?php echo wpautop($business->description); ?>
-                        <?php else : ?>
-                        <p>No description available</p>
-                        <?php endif ?>
-                    </div><!-- /.col-md-9 -->
-                    <div class="bh_details bh_col-md-3">
-                            <div class="bh_section">
-                                <h5>Location</h5>
-                                <p><?php echo $business->address1 ?><br>
-                                <?php echo $business->locality ?>, <?php echo $business->region ?> <?php echo $business->postal_code ?><br>
-                                <a href="https://maps.google.com?daddr=<?php echo urlencode($business->address1) ?>+<?php echo urlencode($business->locality) ?>+<?php echo urlencode($business->region) ?>+<?php echo urlencode($business->postal_code) ?>" target="_blank">Get directions &rarr;</a></p>
-                            </div>
-                            <?php if(false): ?>
-                            <div class="bh_section">
-                                    <h5>Hours</h5>
-                                    <dl class="bh_dl-horizontal">
-                                            <dt>Sun:</dt>
-                                            <dd>Closed</dd>
-                                            <dt>Mon:</dt>
-                                            <dd>Closed</dd>
-                                            <dt>Tue:</dt>
-                                            <dd>9am - 6pm</dd>
-                                            <dt>Wed:</dt>
-                                            <dd>9am - 6pm</dd>
-                                            <dt>Thu:</dt>
-                                            <dd>10am - 9pm</dd>
-                                            <dt>Fri:</dt>
-                                            <dd>9am - 6pm</dd>
-                                            <dt>Sat:</dt>
-                                            <dd>8am - 5pm</dd>
-                                    </dl>
-                            </div>
-                            <?php endif; ?>
-                            <?php if(false): ?>
-                            <div class="bh_section">
-                                    <h5>Contact Us</h5>
-                                    <div class="bh_list-group">
-                                        <a class="bh_list-group-item">Phone: (732) 536-8500</a>
-                                        <a class="bh_list-group-item" href="#">Visit our Website &rarr;</a>
-                                        <a class="bh_list-group-item" href="#">Like us on Facebook &rarr;</a>
-                                        <a class="bh_list-group-item" href="#">Follow us on Twitter &rarr;</a>     
-                                    </div>
-                            </div>
-                            <?php endif; ?>
-                    </div><!-- /.col-md-3 -->
-            </div><!-- /.row -->
+                        <?php if(false): ?>
+                        <a class="bh_list-group-item" href="#">Like us on Facebook &rarr;</a>
+                        <a class="bh_list-group-item" href="#">Follow us on Twitter &rarr;</a>
+                        <?php endif; ?>
+                    </div>
+                </div>
+                <?php endif; ?>
+            </div><!-- /.col-md-3 -->
+        </div><!-- /.row -->
     </div><!-- /.col-md-10 -->
 </div><!-- /.row -->
