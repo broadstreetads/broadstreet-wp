d254b9864e683358cc01e33a73ad2e940e8dbbc3
Eugene Fabrikant <eugene@bizyhood.com>
2016-02-12 16:47:41 -0500

    Bumped release version

--- bizyhood.php
+++ bizyhood.php
@@ -3,7 +3,7 @@
 Plugin Name: Bizyhood
 Plugin URI: https://bizyhood.com
 Description: Integrate Bizyhood business directory on your site
-Version: 1.0.1
+Version: 1.0.2
 Author: Bizyhood
 Author URI: http://bizyhood.com
 */
