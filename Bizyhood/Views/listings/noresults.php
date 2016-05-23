8780eb47e37890f0846036571ac4ff7956e3fcef
spyrosvl <ludwigvb@gmail.com>
2016-05-23 18:37:05 +0300

    Fixed fatal error when there are no promotions to display

---
+++ Bizyhood/Views/listings/noresults.php
@@ -0,0 +1,10 @@
+<div class="row bh_business-header">
+    <div class="col-md-12 bh_business-header-title">	
+        <div class="bh_alert">
+          <p>
+          <?php echo $error; ?>
+          </p>
+        </div>
+    </div>
+    
+</div>
