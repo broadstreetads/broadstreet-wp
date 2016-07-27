c8c98c93abeebc69bd363090c056b899779d7680
Eugene Fabrikant <eugene@bizyhood.com>
2016-07-26 21:07:03 -0400

    Changed the error markup.

--- Bizyhood/Views/listings/error.php
+++ Bizyhood/Views/listings/error.php
@@ -1,13 +1,5 @@
 <div class="row bh_business-header">
     <div class="col-md-8 bh_business-header-title">	
-      <?php if (!(isset($noheader) && $noheader === true)) { ?>
-        <h3>There seems to be a problem!</h3>
-      <?php } ?>
-        <div class="bh_panel bh_alert">
-          <p>
-          <?php echo $error; ?>
-          </p>
-        </div>
+        <h3><?php echo $error; ?></h3>
     </div>
-    
 </div>
