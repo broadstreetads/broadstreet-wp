dc5210b9a7de316b40344d33812afefe7bb50fa3
Spyros Vlachopoulos <ludwigvb@gmail.com>
2016-07-31 22:48:55 +0300

    Added link to the directory on both error.php and noresults.php

--- Bizyhood/Views/listings/noresults.php
+++ Bizyhood/Views/listings/noresults.php
@@ -4,7 +4,9 @@
           <p>
           <?php echo $error; ?>
           </p>
+          <p>
+            <a href="<?php echo get_permalink(Bizyhood_Utility::getOption(Bizyhood_Core::KEY_MAIN_PAGE_ID)); ?>" title="Return to the business directory">Return to the business directory</a>
+          </p>
         </div>
     </div>
-    
 </div>
