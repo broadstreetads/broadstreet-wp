cb3be0f997aec8171167409cdbd691ebe74c81fc
Spyros Vlachopoulos <ludwigvb@gmail.com>
2016-08-03 02:05:33 +0300

    bp-19 Centered link back and increased its font size

--- Bizyhood/Views/listings/noresults.php
+++ Bizyhood/Views/listings/noresults.php
@@ -4,7 +4,7 @@
           <p>
           <?php echo $error; ?>
           </p>
-          <p>
+          <p class="linkback">
             <a href="<?php echo get_permalink(Bizyhood_Utility::getOption(Bizyhood_Core::KEY_MAIN_PAGE_ID)); ?>" title="Return to the business directory">Return to the business directory</a>
           </p>
         </div>
