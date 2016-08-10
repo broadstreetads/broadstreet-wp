77fff8fe275abf319ea223207fa143e72449948a
Eugene Fabrikant <eugene@bizyhood.com>
2016-08-10 13:12:34 -0400

    Bumped version to 1.2.4.

--- bizyhood.php
+++ bizyhood.php
@@ -3,7 +3,7 @@
 Plugin Name: Bizyhood
 Plugin URI: https://bizyhood.com
 Description: Integrates Bizyhood for Publishers engagement and discovery platform to your hyperlocal digital publishing site
-Version: 1.2.3
+Version: 1.2.4
 Author: Bizyhood
 Author URI: http://bizyhood.com
 */
