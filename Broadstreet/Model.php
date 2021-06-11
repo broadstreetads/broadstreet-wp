b01af61a0b111bebd00c8210baffafc37c41b7ac
Kenny Katzgrau <katzgrau@gmail.com>
2021-06-11 16:56:27 -0400

    Remove direct SQL, except for one

--- Broadstreet/Model.php
+++ Broadstreet/Model.php
@@ -11,78 +11,11 @@
  */
 class Broadstreet_Model
 {
-    public static function getPublishedDocument($document_id)
-    {
-        global $wpdb;
-
-        $table = self::getTableName('posts');
-
-        $sql = "SELECT *
-                FROM $table
-                WHERE ID = '$document_id'
-                AND post_status = 'publish'
-                LIMIT 1";
-
-        $posts = $wpdb->get_results($sql);
-
-        if(count($posts) == 0)
-            return false;
-        else
-            return $posts[0];
-    }
-
-    public static function getPublishedDocuments($page = 0, $per_page = 10)
-    {
-        global $wpdb;
-
-        $start = $page * $per_page;
-
-        $table = self::getTableName('posts');
-
-        $sql = "SELECT *
-                FROM $table
-                WHERE post_status = 'publish'
-                LIMIT $start, $per_page";
-
-        return $wpdb->get_results($sql);
-    }
-
-    public static function getPublishedPostCount()
-    {
-        global $wpdb;
-        
-        $table = self::getTableName('posts');
-
-        $sql = "SELECT COUNT(*) AS 'count'
-                FROM $table
-                WHERE post_status = 'publish'";
-
-        $results = $wpdb->get_results($sql);
-
-        return $results[0]->count;
-    }
-
-    public static function getPostComments($post_id, $page = FALSE, $per_page = FALSE)
-    {
-        global $wpdb;
-
-        $table = self::getTableName('comments');
-
-        /* Process posts in batches of 20 from the db */
-        $sql = "SELECT comment_author, comment_content
-                FROM $table
-                WHERE comment_approved = '1'
-                AND comment_post_ID = '$post_id'";
-                
-        if($page && $per_page)
-        {
-            $start = $page * $per_page;
-            $sql = "$sql LIMIT $start, $per_page";
-        }
-
-        return $wpdb->get_results($sql);
-    }
-    
+    /**
+     * There is a more modern way to do this, but this is also used very infrequently.
+     * It's also pretty efficient: it grabs post meta at once so that a loop doesn't
+     * have to be used later
+     */
     public static function getPostMeta($post_ids = array(), $defaults = array())
     {
         if(count($post_ids) == 0) return array();
@@ -91,7 +24,6 @@ class Broadstreet_Model
 
         $table = self::getTableName('postmeta');
 
-        /* Process posts in batches of 20 from the db */
         $sql = "SELECT post_id, meta_key, meta_value
                 FROM $table
                 WHERE post_id IN (".implode(',', $post_ids).")";
@@ -109,13 +41,6 @@ class Broadstreet_Model
         }
         
         return $meta;
-    }
-
-    public static function getCategories()
-    {
-        global $wpdb;
-
-        $table = $this->getTableName('');
     }
 
     public static function getTableName($table_name)
